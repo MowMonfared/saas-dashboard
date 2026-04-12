ed8e7f8855d8770e73477567421c8949dca1ba9b
Mo Monfared <86447387+MowMonfared@users.noreply.github.com>
2026-04-11 21:28:44 -0400

    Build accordion Component

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Plus, ChevronDown } from 'lucide-react';
+import { useState } from 'react';
+import { Plus, ChevronDown, ChevronUp } from 'lucide-react';
 import './Sidebar.css';
 
 export default function Accordion() {
@@ -8,33 +8,45 @@ export default function Accordion() {
   return (
     <>
       <div className="dep">
-        <div className="dep-name"> Departments</div>
-        <ChevronDown className="dep-icon" />
+        <div className="dep-name" onClick={() => setIsOpen(!isOpen)}>
+          {' '}
+          Departments
+        </div>
+        {isOpen ? (
+          <ChevronDown className="dep-icon" />
+        ) : (
+          <ChevronUp className="dep-icon" />
+        )}
       </div>
-      <div className="dep-selected">
-        <div className="dep-name">All Departments</div>
-        <div className="dep-number">14</div>
-      </div>
-      <div className="dep">
-        <div className="dep-name">Engineering</div>
-        <div className="dep-number">5</div>
-      </div>
-      <div className="dep">
-        <div className="dep-name">Design</div>
-        <div className="dep-number">2</div>
-      </div>
-      <div className="dep">
-        <div className="dep-name">Finance</div>
-        <div className="dep-number">2</div>
-      </div>
-      <div className="dep">
-        <div className="dep-name">HR</div>
-        <div className="dep-number">2</div>
-      </div>
-      <button className="add-dep">
-        <Plus className="dep-icon" />
-        Add
-      </button>
+      {isOpen && (
+        <>
+          <div className="dep-selected">
+            <div className="dep-name">All Departments</div>
+            <div className="dep-number">14</div>
+          </div>
+
+          <div className="dep">
+            <div className="dep-name">Engineering</div>
+            <div className="dep-number">5</div>
+          </div>
+          <div className="dep">
+            <div className="dep-name">Design</div>
+            <div className="dep-number">2</div>
+          </div>
+          <div className="dep">
+            <div className="dep-name">Finance</div>
+            <div className="dep-number">2</div>
+          </div>
+          <div className="dep">
+            <div className="dep-name">HR</div>
+            <div className="dep-number">2</div>
+          </div>
+          <button className="add-dep">
+            <Plus className="dep-icon" />
+            Add
+          </button>
+        </>
+      )}
     </>
   );
 }
